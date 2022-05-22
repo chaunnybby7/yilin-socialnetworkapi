cae7b130380957e8c62e64375e23284098dec8c8
YiLin Ong <chauntelleinc@gmail.com>
2022-05-22 18:10:45 -0400

    added delete reaction on thoughts-controller.js

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -98,3 +98,33 @@ removeThought({ params }, res) {
 
 // POST api/thoughts/:id/reactions
 // CREATE reactions
+addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.thoughtId },
+        { $push: { reactions: body }},
+        { new: true, runValidators: true })
+        .then(updatedThought => {
+            if (!updatedThought) {
+                res.status(404).json({ message: 'No reaction found with this id.'});
+                return;
+            }
+            res.json(updatedThought);
+        })
+        .catch(err => res.json(err));
+    },
+// DELETE api/thoughts/:id/reactions
+// Delete reaction 
+removeReaction({ params}, res) {
+    Thought.findOneAndUpdate({ _id: params.thoughtId},
+        { $pull: { reactions: { reactionId: params.reactionId }}},
+        { new: true })
+        .then((thought) => {
+            if (!thought) => {
+                res.status(404).json({ message: 'No reaction found with this ID. ' });
+                return;
+            } res.json(thought)
+        })
+        .catch(err => res.json(err));
+    },
+}
+
+module.exports = thoughtCtrl;
