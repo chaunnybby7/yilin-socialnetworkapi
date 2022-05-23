fcaa056d41da98d8c41d2b08f02d43e3806f270f
YiLin Ong <chauntelleinc@gmail.com>
2022-05-23 19:59:59 -0400

    create user on users-controller.js

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -29,3 +29,28 @@ getUserById({ params }, res) {
         res.status(400).json(err);
     });
 },
+
+//POST /api/users
+// Create user 
+updateUser({
+    params,
+    body
+}, res) {
+    User.findOneAndUpdate({
+            _id: params.id
+        }, body, {
+            new: true,
+            runValidators: true
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({
+                    message: 'No user found with this id.'
+                });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+},
+
