25953ede2ab1b242d6e1db4e5f4d4c8edf127d86
YiLin Ong <chauntelleinc@gmail.com>
2022-05-23 20:56:51 -0400

    fixed some typo errors

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thought-routes');
-const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thoughts-routes');
+const userRoutes = require('./users-routes');
 
 router.use('/thoughts', thoughtRoutes);
 // add `users` to routes created in `user-routes.js`
