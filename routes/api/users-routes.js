997e537ff31ada818463d3bea37f0b9b9bbb3ec7
YiLin Ong <chauntelleinc@gmail.com>
2022-05-23 20:38:48 -0400

    set up get all and get, put, delete one user

---
+++ routes/api/users-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const { getAllUsers, getUserById, createUser, updateUser, deleteUser, addToFriendList, removefromFriendList } = require('../../controllers/users-controller');
+
+// Set up GET all and POST api/users. 
+// Name of controller as cb 
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// Set up GET one, PUT, and DELETE at api/users/id
+router 
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+router
+    .route('/:userId/friends/:friendId')
+    .delete(removefromFriendList)
+
+    module.exports = router;
